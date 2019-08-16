ab120367a3a01803972affc0ddf38abfdfd64ddf
Anh <ltanh2710@gmail.com>
2019-08-16 17:25:08 +0700

    Resize image

--- src/api/resources/images/images.controller.js
+++ src/api/resources/images/images.controller.js
@@ -1,9 +1,39 @@
 import path from 'path';
+import fs from 'fs';
+
+const sharp = require('sharp');
+import { CONSTANTS } from '../../constant/constant';
+
+var sizeOf = require('image-size');
+
 export default {
   async uploadImage(req, res) {
     try {
       const { filename } = req.file;
-      return res.json({ filename: filename });
+      let fileNameAfterResize = 'image-' + filename
+      let properties = sizeOf(CONSTANTS.IMAGE_PATH + filename);
+      const imageHeight = properties.height;
+
+      await sharp(CONSTANTS.IMAGE_PATH + filename)
+        .rotate()
+        .resize(null, imageHeight > 960 ? 960 : null)
+        .toFile(CONSTANTS.IMAGE_PATH + fileNameAfterResize)
+        .then(async (new_file_info) => {
+          fs.unlink(CONSTANTS.IMAGE_PATH + filename, (err) => {
+            if (err) {
+              console.log('err', err);
+              throw err;
+            }
+          });
+          return res.status(200).send({ success: true, image_id: fileNameAfterResize });
+        })
+        .catch(function(err) {
+          return res.status(404).json({
+            success: false,
+            message: 'Không thể tải ảnh lên, vui lòng kiểm tra và thử lại',
+          });
+        });
+
     } catch (err) {
       console.error(err);
       return res.status(500).send(err);
--- src/api/resources/images/images.router.js
+++ src/api/resources/images/images.router.js
@@ -1,121 +1,123 @@
 import express from 'express';
 import imagesController from './images.controller';
-import multer from 'multer'
-import fs from 'fs'
+import multer from 'multer';
+import fs from 'fs';
+import { CONSTANTS } from '../../constant/constant';
 
 // config upload image
 let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/images')
+  destination: function(req, file, cb) {
+    cb(null, CONSTANTS.IMAGE_PATH);
   },
-  filename: function (req, file, cb) {
-    let originalFilename = file.originalname;
-    cb(null, originalFilename  )
-  }
-})
+  filename: function(req, file, cb) {
+    let dotIndex = file.originalname.lastIndexOf('.');
+    let fileName = file.originalname.slice(0, dotIndex);
+    let fileExtension = file.originalname.slice(dotIndex, file.originalname.length);
+    let timestamp = (new Date()).getTime();
+    let nameTimestamp = fileName + '-' + timestamp + fileExtension;
+    cb(null, nameTimestamp);
+  },
+});
 
 function extFile(req, file, cb) {
-  if(!file.originalname.match(/\.(jpg|png|jpeg|gif|JPG|PNG|JPEG)$/)){
-    return cb(new Error('Ảnh không đúng định dạng'))
-  }else{
-    cb(null, true)
+  if (!file.originalname.match(/\.(jpg|png|jpeg|gif|JPG|PNG|JPEG)$/)) {
+    return cb(new Error('Ảnh không đúng định dạng'));
+  } else {
+    cb(null, true);
   }
 }
 
-let uploadImage = multer({ storage: storage, fileFilter: extFile })
+let uploadImage = multer({ storage: storage, fileFilter: extFile });
+
 function checkUploadPath(req, res, next) {
   let path = './uploads/images';
   fs.exists(path, function(exists) {
-    if(exists) {
+    if (exists) {
       next();
-    }
-    else {
+    } else {
       fs.mkdir(path, function(err) {
-        if(err) {
+        if (err) {
           console.log('Error in folder creation');
           next();
         }
         next();
-      })
+      });
     }
-  })
+  });
 }
 
 // kết thúc config upload image
 
 
-
 // config upload file
 let storageFiles = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/files')
+  destination: function(req, file, cb) {
+    cb(null, './uploads/files');
   },
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     let originalFilename = file.originalname;
-    cb(null, originalFilename  )
-  }
-})
+    cb(null, originalFilename);
+  },
+});
 
 function extFileFiles(req, file, cb) {
-  if(!file.originalname.match(/\.(doc|docx|xls|xlsx|excel|pdf)$/)){
-    return cb(new Error('Tệp tin không đúng định dạng'))
-  }else{
-    cb(null, true)
+  if (!file.originalname.match(/\.(doc|docx|xls|xlsx|excel|pdf)$/)) {
+    return cb(new Error('Tệp tin không đúng định dạng'));
+  } else {
+    cb(null, true);
   }
 }
 
-let uploadFile = multer({ storage: storageFiles, fileFilter: extFileFiles })
+let uploadFile = multer({ storage: storageFiles, fileFilter: extFileFiles });
 
 function checkUploadPathFiles(req, res, next) {
   let path = './uploads/files';
   fs.exists(path, function(exists) {
-    if(exists) {
+    if (exists) {
       next();
-    }
-    else {
+    } else {
       fs.mkdir(path, function(err) {
-        if(err) {
+        if (err) {
           console.log('Error in folder creation');
           next();
         }
         next();
-      })
+      });
     }
-  })
+  });
 }
 
 // kết thúc config upload Files
 
 
-
 // config upload avatar
 let storageAvatar = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/avatar')
+  destination: function(req, file, cb) {
+    cb(null, './uploads/avatar');
   },
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     let originalFilename = file.originalname;
-    cb(null, originalFilename  )
-  }
-})
+    cb(null, originalFilename);
+  },
+});
 
-let uploadAvatar = multer({ storage: storageAvatar, fileFilter: extFile })
+let uploadAvatar = multer({ storage: storageAvatar, fileFilter: extFile });
+
 function checkUploadPathAvatar(req, res, next) {
   let path = './uploads/avatar';
   fs.exists(path, function(exists) {
-    if(exists) {
+    if (exists) {
       next();
-    }
-    else {
+    } else {
       fs.mkdir(path, function(err) {
-        if(err) {
+        if (err) {
           console.log('Error in folder creation');
           next();
         }
         next();
-      })
+      });
     }
-  })
+  });
 }
 
 // kết thúc config upload avatar
@@ -124,37 +126,36 @@ function checkUploadPathAvatar(req, res, next) {
 function checkUploadPathFolder(req, res, next) {
   let path = './uploads';
   fs.exists(path, function(exists) {
-    if(exists) {
+    if (exists) {
       next();
-    }
-    else {
+    } else {
       fs.mkdir(path, function(err) {
-        if(err) {
+        if (err) {
           console.log('Error in folder creation');
           next();
         }
         next();
-      })
+      });
     }
-  })
+  });
 }
 
 export const imagesRouter = express.Router();
 
 imagesRouter
   .route('/')
-  .post(checkUploadPathFolder, checkUploadPathFiles, uploadFile.single('image'), imagesController.uploadImage)
+  .post(checkUploadPathFolder, checkUploadPathFiles, uploadFile.single('image'), imagesController.uploadImage);
 
 imagesRouter
   .route('/image')
-  .post(checkUploadPathFolder, checkUploadPath, uploadImage.single('image'), imagesController.uploadImage)
+  .post(checkUploadPathFolder, checkUploadPath, uploadImage.single('image'), imagesController.uploadImage);
 
 imagesRouter
   .route('/avatar')
-  .post(checkUploadPathFolder, checkUploadPathAvatar, uploadAvatar.single('image'), imagesController.uploadImage)
+  .post(checkUploadPathFolder, checkUploadPathAvatar, uploadAvatar.single('image'), imagesController.uploadImage);
 
-imagesRouter.get('/:fileNm', imagesController.getFileByName)
-imagesRouter.get('/image/:imgNm', imagesController.getImageByName)
-imagesRouter.get('/avatar/:imgNm', imagesController.getAvatarByName)
+imagesRouter.get('/:fileNm', imagesController.getFileByName);
+imagesRouter.get('/image/:imgNm', imagesController.getImageByName);
+imagesRouter.get('/avatar/:imgNm', imagesController.getAvatarByName);
 
 
